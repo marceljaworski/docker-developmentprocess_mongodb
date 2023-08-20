3a01035b3429e54f6006f7b59c58499e9dece4b9
Marcelearn <jaworskimarcel@gmail.com>
2023-08-20 12:34:09 +0200

    update

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Docker Development Process
+We will have a JavaScript Frontend, Nodeexpress Backend and Docker MongoDB database.
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+
+app.get('/', function (req, res) {
+  var img = fs.readFileSync('profile-1.jpg');
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+})
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000")
+})
