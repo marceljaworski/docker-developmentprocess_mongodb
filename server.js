dbc2717cd799496d829e563142d7a7a9aaffd8b2
Marcelearn <jaworskimarcel@gmail.com>
2023-08-21 16:55:45 +0200

    picture endpoint added

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/profile-picture', function (req, res) {
   var img = fs.readFileSync('profile-1.jpg');
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
-});
+})
 
 app.get('/get-profile', function (req, res) {
   var response = res;
