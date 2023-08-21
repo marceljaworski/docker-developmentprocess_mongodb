79f88acce387f50962f370d423e3132fa5b41148
Marcelearn <jaworskimarcel@gmail.com>
2023-08-21 17:49:02 +0200

    picture fixed

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { join } from 'path';
+import path from 'path';
 import { readFileSync } from 'fs';
 import { MongoClient } from 'mongodb';
 import bodyParser from 'body-parser';
 var app = express();
+const __dirname = path.resolve();
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -12,22 +13,18 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.sendFile(join(__dirname, "index.html"));
+
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/profile-picture', function (req, res) {
-  var img = readFileSync('profile-1.jpg');
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
-})
 
 app.get('/get-profile', function (req, res) {
   var response = res;
 
-  MongoClient.connect('mongodb://admin:password@localhst:27017', function (err, client) {
+  MongoClient.connect('mongodb://admin:password@localhost:27017', function (err, client) {
     if (err) throw err;
 
-    var db = client.db('user-acount');
+    var db = client.db('user-account');
     var query = { userid: 1 };
     db.collection('users').findOne(query, function (err, result) {
       if (err) throw err;
@@ -41,10 +38,10 @@ app.post('/update-profile', function (req, res) {
   var userObj = req.body;
   var response = res;
   console.log('connecting to the db...')
-  MongoClient.connect('mongodb://admin:password@localhst:27017', function (err, client) {
+  MongoClient.connect('mongodb://admin:password@localhost:27017', function (err, client) {
     if (err) throw err;
 
-    var db = client.db('user-acount');
+    var db = client.db('user-account');
     userObj['userid'] = 1;
     var query = { userid: 1 };
     var newValues = { $set: userObj };
@@ -53,13 +50,18 @@ app.post('/update-profile', function (req, res) {
 
     db.collection('users').updateOne(query, newValues, {upsert: true}, function (err, result) {
       if (err) throw err;
-      console.log('succesfully updated');
+      console.log('succesfully updated or inserted');
       client.close();
       response.send(userObj);
     });
   });
 })
 
+app.get('/profile-picture', function (req, res) {
+  var img = readFileSync('profile-1.jpg');
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+})
 
 app.listen(3000, function () {
   console.log("app listening on port 3000")
