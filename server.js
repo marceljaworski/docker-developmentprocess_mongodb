846303500b55d8cbaa0ce6c360c78c17875962f1
Marcelearn <jaworskimarcel@gmail.com>
2023-08-21 16:53:07 +0200

    update profile

--- server.js
+++ server.js
@@ -33,6 +33,28 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
+app.post('/update-profile', function (req, res) {
+  var userObj = req.body;
+  var response = res;
+  console.log('connecting to the db...')
+  MongoClient.connect('mongodb://admin:password@localhst:27017', function (err, client) {
+    if (err) throw err;
+
+    var db = client.db('user-acount');
+    userObj['userid'] = 1;
+    var query = { userid: 1 };
+    var newValues = { $set: userObj };
+
+    console.log('connected to the user-acount db')
+
+    db.collection('users').updateOne(query, newValues, {upsert: true}, function (err, result) {
+      if (err) throw err;
+      console.log('succesfully updated');
+      client.close();
+      response.send(userObj);
+    });
+  });
+})
 
 
 app.listen(3000, function () {
